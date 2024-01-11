94c56503ea589aa93d152a413158bd6c424a3157
universe <hello.its.universe@gmail.com>
2024-01-11 15:10:44 +0900

    chap02 마무리, chap03 추가
    chap02 마무리, chap03 추가

---
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application1.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section01.method;
+
+public class Application1 {
+    public static void main(String[] args) {
+
+        /* 수업목표. 메소드의 호출 흐름에 대해 이해할 수 있다. */
+
+        /* 필기.
+         *  메소드란?
+         *  메소드(method)는 어떤 특정 작업을 수행하기 위한 명령문의 집합이라고 할 수 있다.
+        * */
+    }
+}
