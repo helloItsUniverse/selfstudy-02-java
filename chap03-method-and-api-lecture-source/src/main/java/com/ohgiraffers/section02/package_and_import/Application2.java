ffca83e51a76cc5dbb249582693f9c7a4f76e9b4
universe <hello.its.universe@gmail.com>
2024-01-12 12:27:43 +0900

    sect02 수정, sect03(Math), sect04(스캐너) 추가
    sect02(패키지) App2 수정, sect03(Math), sect04(스캐너) 추가

--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application2.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section02/package_and_import/Application2.java
@@ -7,8 +7,13 @@ public class Application2 {
     /* 수업목표. import에 대해 이해할 수 있다. */
 
     public static void main(String[] args) {
+
+        /* 목차. 1. non-static method의 경우*/
         Calculator cal = new Calculator();
         int sub = cal.subTwoNumbers(80, 21);
         System.out.println("80 - 21 = " + sub);
+
+        /* 목차. 2. static method의 경우 */
+        System.out.println("두 수 중 큰 값은: " + Calculator.maxNumberOf(22, 80));
     }
 }
