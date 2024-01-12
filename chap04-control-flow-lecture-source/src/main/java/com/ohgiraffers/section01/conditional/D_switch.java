853e82a8c7218643cc8c466db2e097753dd95829
universe <hello.its.universe@gmail.com>
2024-01-12 17:06:46 +0900

    sect01 switch 수정
    sect01 switch 수정

--- chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/D_switch.java
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/D_switch.java
@@ -7,18 +7,24 @@ public class D_switch {
 
         /* 수업목표. switch만 단독 사용 시 흐름을 이해하고 적용할 수 있다. */
         Scanner sc = new Scanner(System.in);
-        System.out.println("본인의 티어를 입력하시오(G: 골드, S: 실버, B: 브론즈): ");
+        System.out.print("본인의 티어를 입력하시오(G: 골드, S: 실버, B: 브론즈): ");
         char grade = sc.next().charAt(0);
         int point = 0;
+        boolean flag = true;
+
         switch(grade) {
             case 'G': point += 10;
             case 'S': point += 10;
             case 'B': point += 10;
                       break;
             default:
+                flag = false;
                 System.out.println("어디 회원 티어세요?");
         }
-
-        System.out.println("당신의 티어는 " + grade + "이며, 현재 포인트는 " + point + "입니다.");
+        if(flag) {
+            System.out.println("당신의 티어는 " + grade + "이며, 현재 포인트는 " + point + "입니다.");
+        } else {
+            System.out.println("그럼 이 참에 회원가입 하시죠! ㅎ");
+        }
     }
 }
