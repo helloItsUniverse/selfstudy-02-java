c45f1cc199b20ce723b1c7ac6b36afec5887b1c4
universe <hello.its.universe@gmail.com>
2024-01-16 10:47:26 +0900

    sect02 마무리
    sect02 마무리

--- java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_11659.java
+++ java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_11659.java
@@ -37,7 +37,7 @@ public class boj_ex_11659 {
         int[] arrOut = new int[M];
 
         for (int i = 1; i <= N; i++) {
-//            System.out.println("총 "+ N + "번째 중 " + (i + 1) + "번째 숫자 입력" );
+
             arr[i] = arr[i-1] + sc.nextInt();
         }
 //        System.out.println(Arrays.toString(arr));
