b90f7f2eb8d99ffde3bc911aae76b7750e088977
universe <hello.its.universe@gmail.com>
2024-01-26 14:11:08 +0900

    ch15 람다 수정
    ch15 람다 수정

---
+++ java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_10950.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.baekjoon_exercise;
+
+import java.util.Scanner;
+
+public class boj_ex_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < sc.nextInt(); i++) {
+            
+        }
+    }
+}
