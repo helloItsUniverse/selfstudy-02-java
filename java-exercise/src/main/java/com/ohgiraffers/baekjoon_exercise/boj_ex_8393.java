066c5ef6f20763b6b861f11362ab514844bb3791
universe <hello.its.universe@gmail.com>
2024-01-26 12:49:00 +0900

    ch15 람다 추가
    ch15 람다 추가

---
+++ java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_8393.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.baekjoon_exercise;
+
+import java.util.Scanner;
+
+public class boj_ex_8393 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num * (num + 1) / 2);
+    }
+}
