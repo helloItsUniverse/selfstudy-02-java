c6427fb8b48e328ffcee108ffe5d2eaea0ec354b
universe <hello.its.universe@gmail.com>
2024-01-31 15:27:04 +0900

    ch17, 백준 연습문제 추가
    ch17, 백준 연습문제 추가

---
+++ java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_15552.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.baekjoon_exercise;
+
+import java.io.*;
+
+public class boj_ex_15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+    }
+}
---
+++ java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_25304.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.baekjoon_exercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_ex_25304 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum += a * b;
+        }
+
+        if (X == sum) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
