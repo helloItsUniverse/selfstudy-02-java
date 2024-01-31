759687dc9692a6f76780e537dfdf9d35ccf255db
universe <hello.its.universe@gmail.com>
2024-01-31 13:18:36 +0900

    ch17, ch00(파라미터화), 연습문제 추가
    ch17, ch00(파라미터화), 연습문제 추가

--- java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_10950.java
+++ java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_10950.java
@@ -4,9 +4,17 @@ import java.util.Scanner;
 
 public class boj_ex_10950 {
     public static void main(String[] args) {
+        int a, b;
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < sc.nextInt(); i++) {
-            
+        int n = sc.nextInt();
+        int[] sumArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            sumArr[i] = a + b;
+        }
+        for (int sum: sumArr) {
+            System.out.println(sum);
         }
     }
 }
---
+++ java-exercise/src/main/java/com/ohgiraffers/teamStudy/ts_0126_Member_Management_2/aggregate/BloodType.java
@@ -0,0 +1,5 @@
+package com.ohgiraffers.teamStudy.ts_0126_Member_Management_2.aggregate;
+
+public enum BloodType {
+    A, AB, B, O;
+}
