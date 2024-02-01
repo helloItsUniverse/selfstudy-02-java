0a83277ea6fedecd4b433f22faa433493056b7c6
universe <hello.its.universe@gmail.com>
2024-02-01 15:38:38 +0900

    ch17-tree,dp 추가 / 백준 문제 추가
    ch17-tree,dp 추가 / 백준 문제 추가

--- java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_10818.java
+++ java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_10818.java
@@ -9,6 +9,18 @@ public class boj_ex_10818 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int max = arr[0], min = arr[0];
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+        bw.write(((Integer) min).toString() + " " + ((Integer) max).toString());
+        bw.flush();
+        bw.close();
     }
 }
---
+++ java-exercise/src/main/java/com/ohgiraffers/baekjoon_exercise/boj_ex_2562.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.baekjoon_exercise;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj_ex_2562 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    }
+}
