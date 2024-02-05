885d3cae7eda5bfbf51ca87e7bb98161d1533950
universe <hello.its.universe@gmail.com>
2024-02-05 15:52:49 +0900

    jdbc ch02 ch03 마무리
    jdbc ch02 ch03 마무리

---
+++ 02_jdbc/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section01/insert/Menu.java
@@ -0,0 +1,70 @@
+package com.ohgiraffers.section01.insert;
+
+public class Menu {
+
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+    private int categoryCode;
+    private String orderableStatus;
+
+    // 기본생성자 + 매개변수있는 생성자 + getter + setter
+
+
+    public Menu() {
+    }
+
+    public Menu(String menuName, int menuPrice, int categoryCode, String orderableStatus) {
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.categoryCode = categoryCode;
+        this.orderableStatus = orderableStatus;
+    }
+
+    public int getMenuCode() {
+        return menuCode;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public String getOrderableStatus() {
+        return orderableStatus;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public void setMenuPrice(int menuPrice) {
+        this.menuPrice = menuPrice;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public void setOrderableStatus(String orderableStatus) {
+        this.orderableStatus = orderableStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "menuCode=" + menuCode +
+                ", menuName='" + menuName + '\'' +
+                ", menuPrice=" + menuPrice +
+                ", categoryCode=" + categoryCode +
+                ", orderableStatus='" + orderableStatus + '\'' +
+                '}';
+    }
+}
