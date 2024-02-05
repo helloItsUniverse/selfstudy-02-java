d2b21b51cff25e05fbda1a993801c140721073af
universe <hello.its.universe@gmail.com>
2024-02-05 12:41:40 +0900

    ch02 prepared 추가
    ch02 prepared 추가

--- 02_jdbc/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/common/JDBCTemplate.java
+++ 02_jdbc/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/common/JDBCTemplate.java
@@ -2,9 +2,7 @@ package com.ohgiraffers.common;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 /* 설명. JDBC를 위한 메소드만 따로 모듈화(Connection 객체 생성, close메소드 처리(Connection, Statement, PreparedStatement)) */
@@ -42,6 +40,26 @@ public class JDBCTemplate {
         try {
             if (con != null && !con.isClosed()) {
                 con.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void close(Statement stmt) {
+        try {
+            if (stmt != null && !stmt.isClosed()) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void close(ResultSet rset) {
+        try {
+            if (rset != null && !rset.isClosed()) {
+                rset.close();
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
